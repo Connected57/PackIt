84b33424c5d015d4ad7dcb05432023c431d70cfc
Aaron Hodgson <hodgson.aaron@outlook.com>
2015-01-21 11:36:39 -0800

    second commit

---
+++ Desktop/PackIt/PackIt/myViewController2ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  myViewController2ViewController.swift
+//  PackIt
+//
+//  Created by Aaron Hodgson on 1/21/15.
+//  Copyright (c) 2015 Aaron Hodgson. All rights reserved.
+//
+
+import UIKit
+
+class myViewController2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        println("Hello")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
